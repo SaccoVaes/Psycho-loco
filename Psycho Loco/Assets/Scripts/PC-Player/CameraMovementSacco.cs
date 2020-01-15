40baeebb290d5e6a7e23df0ccf6573eb657a1dac
SaccoVaes <sacco.vaes@student.aventus.nl>
2020-01-15 17:10:20 +0100

    Push 15 Januari
    -UI elements
    -Movement
    -Multiplayer

---
+++ Psycho Loco/Assets/Scripts/PC-Player/CameraMovementSacco.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementSacco : MonoBehaviour
+{
+    public GameObject camerarig;
+    public GameObject camera;
+ private float currentZoom = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            camera.transform.Translate(0, 0, 1,Space.World);
+            //Vector3 v = new Vector3();
+            //camerarig.transform.Translate();
+        }
+
+        if (Input.GetKey("s"))
+        {
+            camera.transform.Translate(0, 0, -1,Space.World);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            camera.transform.Translate(-1, 0, 0, Space.World);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            camera.transform.Translate(1, 0, 0, Space.World);
+        }
+
+        //checks if the user scrolls up AKA zooming in!
+
+        if (Input.mouseScrollDelta.y > 0 )
+        {
+            camera.transform.Translate(0, 0, 1,Space.Self);
+        }
+
+        //checks if the user scrolls down
+        if (Input.mouseScrollDelta.y < 0 && camera.transform.rotation.eulerAngles.y <= 5.0)
+        {
+            camera.transform.Translate(0, 0, -1, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            camera.transform.Rotate(new Vector3(0,1,0),Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            camera.transform.Rotate(new Vector3(0, -1, 0), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.Z) && camera.transform.rotation.eulerAngles.x < 90)
+        {
+            camera.transform.Rotate(new Vector3(1, 0, 0), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.X)&& camera.transform.rotation.eulerAngles.x > 45)
+        {
+            camera.transform.Rotate(new Vector3(-1, 0, 0), Space.World);
+        }
+
+
+    }
+    // Returns a Vector3 with only the X and Z components (Y is 0'd)
+    public Vector3 vector3XZOnly(Vector3 vec)
+    {
+        return new Vector3(vec.x, 0f, vec.z);
+    }
+}
---
+++ Psycho Loco/Assets/Scripts/UI/InventoryManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+///     Script for 
+/// </summary>
+public class InventoryManager : MonoBehaviour
+{
+    public List<GameObject> InventoryItems = new List<GameObject>();
+    public GameObject SelectedItem;
+
+    void Start()
+    {
+        ActivateHotkeys();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if List is not empty
+        if (InventoryItems.Count != 0)
+        {
+            for(int i = 0; i < InventoryItems.Count; i++)
+            {
+                if (Input.GetKeyDown((i + 1).ToString()))
+                {
+                    OnItemSelected(InventoryItems[i]);
+                }
+            }
+        }
+    }
+
+    //Method for generating hotkeys.
+    public void ActivateHotkeys()
+    {
+        if (InventoryItems.Count != 0)
+        {
+            //For x items in the list.
+            for (int i = 0; i < InventoryItems.Count; i++)
+            {
+                //Get the required text components in the children.
+                var text = InventoryItems[i].GetComponentInChildren<Text>();
+                text.text = (i + 1).ToString();
+            }
+        }
+    }
+
+    public void OnItemSelected(GameObject item)
+    {
+        //Deselect the old item.
+        if (SelectedItem != null)
+        {
+            SelectedItem.GetComponent<Image>().color = Color.white;
+        }
+
+        //Check if the same item is already selected;
+        if (SelectedItem == item)
+        {
+            SelectedItem.GetComponent<Image>().color = Color.white;
+            SelectedItem = null;
+            return;
+        }
+        //Select new item to green;
+            SelectedItem = item;
+            SelectedItem.GetComponent<Image>().color = Color.green;
+        
+    }
+
+}
